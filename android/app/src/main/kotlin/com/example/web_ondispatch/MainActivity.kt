05cdd4229a2f99dc8bd40c3ca1b5a96a6036e531
AvaiyaKashyap <avaiya.kashyap@gmail.com>
2023-07-18 11:44:11 +0530

    webview

---
+++ android/app/src/main/kotlin/com/example/web_ondispatch/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_ondispatch
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:web_ondispatch/web.dart';
+void main()
+{
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(useMaterial3: true),
+    home: webpage(),
+  ));
+}
